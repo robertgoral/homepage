870e1887c04e108dda66ef42990a5e5dd7664dca
Robert Góral <robertgoral@me.com>
2019-07-30 15:34:31 +0200

    lesson-js:eventy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.log('siema!');
-const name='Robert';
-const js='konsoli';
-const age= 75;
+const name = 'Robert';
+const js = 'konsoli';
+const age = 75;
 console.log(`Sie ma jestem ${name} witam
 w ${js} Powodzenia 👍`);
 const diary = document.querySelector('.diary__paragraf--js');
@@ -13,12 +13,12 @@ console.log(paragraph);
 console.log(paragraph[0]);
 const span = document.querySelector('.span__js');
 span.innerHTML = '<i>JS</i> jest Niesamowity';
-if ('javascript'!='java') {
+if ('javascript' != 'java') {
     console.log('to prawda');
 }
-if (age < 20) { 
+if (age < 20) {
     console.log('masz mniej niż 20 lat');
-} else if((age >=20) && (age<=30)) {
+} else if ((age >= 20) && (age <= 30)) {
     console.log('masz więcej niż dwadzieścia a mniej niż 30');
 } else {
     console.log('jesteś 30+');
@@ -27,19 +27,19 @@ if (age < 20) {
 switch (age) {
     case 20:
         console.log('masz równo 20 lat');
-    break;
+        break;
     case 30:
         console.log('masz równo 30 lat');
-    break;
+        break;
     default:
         console.log(`masz ${age} lat`);
-    break;
+        break;
 }
 let oldIndicator;
 
 if (age > 70) {
     oldIndicator = 'yes';
-}else {
+} else {
     oldIndicator = 'no';
 }
 
@@ -49,22 +49,28 @@ console.log(amIOld);
 function calculate(x) {
     x = x + 3;
     console.log(x);
-    return x*7
+    return x * 7
 }
 
 console.log(calculate(2));
 const myCalculation = calculate(4);
 console.log(myCalculation);
-function hellovisitor (name,age) {
-    console.log( `witaj ${name} masz ${age}`);
+function hellovisitor(name, age) {
+    console.log(`witaj ${name} masz ${age}`);
 }
 hellovisitor('radek', '29');
 
-const button = document.querySelector ('.header__button--js');
+const button = document.querySelector('.header__button--js');
 console.log(button);
 
 button.addEventListener('click', (e) => {
     const header = document.querySelector('.header__title--js');
+    //header.classList.add ("header__title--red");
+    if (header.classList.contains("header__title--red")); {
+        console.log('jest klasa');
+    } else {
+        console.log('brak klasy');
+    }
     header.innerHTML = 'Klik Klik';
 
 });
